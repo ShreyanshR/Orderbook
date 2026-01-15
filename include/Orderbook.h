0b9d6bad22444011fdb0d1645dee41d8c064288f
ShreyanshR <shreyanshrathore31@gmail.com>
2026-01-15 23:46:40 +0100

    Prune orders at the EOD

--- include/Orderbook.h
+++ include/Orderbook.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <map>
 #include <unordered_map>
 #include <thread>
@@ -10,3 +11,67 @@
 #include "Order.h"
 #include "OrderbookLevelInfos.h"
 #include "Trade.h"
+#include "OrderModify.h"
+
+
+class OrderBook
+{
+    private:
+        struct OrderEntry
+        {
+            OrderPointer order_{ nullptr }; // Actual order object
+            OrderPointers::iterator location_; // Iterator pointing to its positin in bids_/askç
+        };
+
+        struct LevelData
+        {
+            Quantity quantity_{ }; //Total Quantity as this price
+            Quantity count_{ }; //Numbers of orders at this price
+
+            enum class Action
+            {
+                Add, //Order Added
+                Remove, // Order Removed
+                Match, // Order Matched/Filled
+            };
+        };
+
+        std::unordered_map<Price, LevelData> data_;
+        std::map<Price, OrderPointers, std::greater<Price>> bids_; //best bid on the top, highest bid at top
+        std::map<Price, OrderPointers, std::less<Price>> asks_; // Best ask, lowest bid at top
+        std::unordered_map<OrderId, OrderEntry> orders_;
+        mutable std::mutex ordersMutex_;
+        std::thread ordersPruneThread_;
+        std::condition_variable shutdownConditionVariable_;
+        std::atomic<bool> shutdown_{ false };
+
+        void PruneGoodForDayOrder();
+        void CancelOrders(OrderIds orderIds);
+        void CancelOrderInternal(OrderId orderId);
+
+        void OnOrderCancelled(OrderPointer order);
+        void OnOrderAdded(OrderPointer order);
+        void OnOrderMatched(Price price, Quantity quantity, bool isFullyFilled);
+        void UpdateLevelData(Price price, Quantity quantity, LevelData::Action action);
+
+        bool CanFullyFill(Side side, Price price, Quantity quantity) const;
+        bool CanMatch(Side side, Price price) const;
+        Trades MatchOrders();
+
+
+
+    public:
+        OrderBook();
+        OrderBook(const OrderBook&) = delete;
+        void operator=(const OrderBook&) = delete;
+        OrderBook(OrderBook&&) = delete;
+        void operator=(OrderBook&&) = delete;
+        ~OrderBook();
+
+        Trades AddOrder(OrderPointer order);
+        void CancelOrder(OrderId orderId);
+        Trades ModifyOrder(OrderModify order);
+
+        std::size_t Size() const;
+        OrderBookLevelInfos GetOrderInfos() const;
+};
--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,163 +1,60 @@
-#include <cstdint>
-#include <format>
-#include <functional>
-#include <list>
-#include <memory>
-#include <stdatomic.h>
-#include <stdexcept>
-#include <unordered_map>
-#include <vector>
-#include <map>
+#include "Orderbook.h"
 
-enum class OrderType
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <numeric>
+#include <chrono>
+#include <ctime>
+
+
+void OrderBook::PruneGoodForDayOrder()
 {
-    GoodTillCancel,
-    FillAndKill
-};
+    using namespace std::chrono;
+    const auto end = hours(16);
 
-enum class Side
-{
-    Buy,
-    Sell
-};
-
-using Price = std::int32_t;
-using Quantity = std::uint32_t;
-using OrderId = std::uint64_t;
-
-struct LevelInfo
-{
-    Price price_;
-    Quantity quantity_;
-};
-
-using LevelInfos = std::vector<LevelInfo>;
-
-class OrderBookLevelInfos
-{
-private:
-    LevelInfo bids_;
-    LevelInfos asks_;
-
-public:
-    OrderBookLevelInfos(const LevelInfo& bids, const LevelInfos& asks):
-        bids_{bids}, asks_{asks}
-    { }
-
-    const LevelInfo& GetBids() const{return bids_;}
-    const LevelInfos& GetAsks() const{return asks_;}
-};
-
-class Order
-{
-public:
-    Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
-        : orderType_{ orderType }
-        , orderId_{ orderId }
-        , side_{ side }
-        , price_{ price }
-        , initialQuantity_ { quantity }
-        , remaningQuantity_{ quantity }
-    { }
-
-    OrderType GetOrderType() const { return orderType_; }
-    OrderId GetOrderId() const { return orderId_;}
-    Side GetSide() const { return side_; }
-    Price GetPrice() const { return price_;}
-    Quantity GetInitialQuantity() const {return initialQuantity_; }
-    Quantity GetRemaningQuantity() const {return remaningQuantity_; }
-    Quantity GetFilledQuantity() const {return GetInitialQuantity() - GetRemaningQuantity(); }
-
-    void Fill(Quantity quantity)
+    while(true)
     {
-        if (quantity > GetFilledQuantity())
-            throw std::logic_error(std::format("Order ({}) cannot be filled for more than it's remaning Quantity.", GetOrderId()));
-        remaningQuantity_ -= quantity;
+        const auto now = system_clock::now();
+        const auto now_c = system_clock::to_time_t(now);
+
+        std::tm now_parts;
+
+        localtime_r(&now_c, &now_parts); // convert the current time into human readable time
+
+        if(now_parts.tm_hour >= end.count())
+            now_parts.tm_mday += 1;
+
+        now_parts.tm_hour = end.count();
+        now_parts.tm_min = 0;
+        now_parts.tm_sec = 0;
+
+        auto next = system_clock::from_time_t(mktime(&now_parts));
+        auto till = next - now + milliseconds(100);
+
+
+        {
+            std::unique_lock ordersLock{ ordersMutex_};
+
+            if (shutdown_.load(std::memory_order_acquire) || shutdownConditionVariable_.wait_for(ordersLock, till) == std::cv_status::no_timeout)
+                return;
+        }
+
+        OrderIds orderIds;
+
+        {
+            std::scoped_lock ordersLock{ ordersMutex_}; 
+
+            for(const auto& [orderId, entry] : orders_)
+            {
+                const auto& [order, location] = entry;
+                if (order->GetOrderType() != OrderType::GoodForDay)
+                    continue;
+
+                orderIds.push_back(order->GetOrderId());
+            }
+        }
+        CancelOrders(orderIds);
+
     }
-
-private:
-    OrderType orderType_;
-    OrderId orderId_;
-    Side side_;
-    Price price_;
-    Quantity initialQuantity_;
-    Quantity remaningQuantity_;
-};
-
-
-using OrderPointer = std::shared_ptr<Order>;
-using OrderPointers = std::list<OrderPointer>;
-
-struct TradeInfo
-{
-    OrderId orderId_;
-    Price price_;
-    Quantity quantity_;
-};
-
-class Trade
-{
-    public:
-        Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
-            : bidTrade_{ bidTrade }
-            , askTrade_{ askTrade }
-        { }
-
-        const TradeInfo& GetBidTrade() const { return bidTrade_; }
-        const TradeInfo& GetAskTrade() const { return askTrade_; }
-
-    private:
-        TradeInfo bidTrade_;
-        TradeInfo askTrade_;
-
-};
-
-using Trades = std::vector<Trade>;
-
-class Orderbook
-{
-    private:
-        struct OrderEntry
-        {
-            OrderPointer order_{ nullptr };
-            OrderPointers::iterator location_;
-        };
-
-        std::map<Price, OrderPointers, std::greater<Price>> bids_;
-        std::map<Price, OrderPointers, std::less<Price>> asks_;
-        std::unordered_map<OrderId, OrderEntry> orders_;
-
-        bool CanMatch(Side side, Price price) const
-        {
-            if (side == Side::Buy)
-            {
-                if (asks_.empty())
-                    return false;
-
-                const auto& [bestAsk, _] = *asks_.begin();
-                return price >= bestAsk;
-            }
-            else {
-            {
-                if (bids_.empty())
-                    return false;
-                const auto& [bestBid, _] = *bids_.begin();
-                return price <= bestBid;
-            }
-            }
-        }
-
-        Trades MatchOrders()
-        {
-            Trades trades;
-            trades.reserve(orders_.size()); 
-
-            while (true)
-            {
-
-            }
-        }
-
-
-    
 };
